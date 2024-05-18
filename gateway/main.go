97fbba3030c77373512eb0d9fe51900dbadf5929
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-18 15:15:19 +0630

    feat(): grpc svc connection setup

--- gateway/main.go
+++ gateway/main.go
@@ -5,15 +5,26 @@ import (
 	"net/http"
 
 	"github.com/kaungmyathan22/golang-order-management-system-microservice/common"
+	"github.com/kaungmyathan22/golang-order-management-system-microservice/common/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
+	httpAddr     = common.EnvString("HTTP_ADDR", ":8080")
+	orderService = common.EnvString("ORDER_SERVICE", "localhost:3000")
 )
 
 func main() {
+	conn, err := grpc.Dial(orderService, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("failed to dial server %v", orderService)
+	}
+	defer conn.Close()
+	log.Fatal("dialing order service at %v", orderService)
+	c := api.NewOrderServiceClient(conn)
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoute(mux)
 	log.Printf("server is running at http://localhost%s\n", httpAddr)
 	if err := http.ListenAndServe(httpAddr, mux); err != nil {
