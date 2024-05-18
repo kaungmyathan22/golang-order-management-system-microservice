da9fa007b0c09a3d0ac1dca414a3e7bdc1b5dc44
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-18 15:31:51 +0630

    feat(): grpc server setup

--- orders/main.go
+++ orders/main.go
@@ -1,9 +1,29 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/kaungmyathan22/golang-order-management-system-microservice/common"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = common.EnvString("GRPC_ADDR", "localhost:2000")
+)
 
 func main() {
+	grpcServer := grpc.NewServer()
+	l, err := net.Listen(grpcAddr, grpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	defer l.Close()
 	store := NewStore()
 	svc := NewService(store)
 	svc.CreateOrder(context.Background())
+	if err = grpcServer.Serve(l); err != nil {
+		log.Fatal(err.Error())
+	}
 }
